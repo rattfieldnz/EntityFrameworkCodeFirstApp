8cdeaa0b81d319faef796f1af9018e74a1505f07
= <attfire1@student.op.ac.nz>
2013-10-09 13:35:12 +1300

    Added icon to interface

--- attfire1_assignment2/attfire1_assignment2/Form1.Designer.cs
+++ attfire1_assignment2/attfire1_assignment2/Form1.Designer.cs
@@ -134,6 +134,7 @@
             this.Controls.Add(this.mainBannerLogo);
             this.Controls.Add(this.mainTabGroup);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(1015, 1000);
             this.MinimumSize = new System.Drawing.Size(1015, 726);
             this.Name = "Form1";
